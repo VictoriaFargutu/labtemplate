84eb3ab8388843d0947896500fca1cb3e21bc56e
Andreea Vantu <vantuandreea@gmail.com>
2018-01-14 13:17:13 +0200

    Login and register inputs

--- frontend/src/app/components/pages/sign-in/sign-in.component.ts
+++ frontend/src/app/components/pages/sign-in/sign-in.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  username:string;
+  password:string;
+  firstname:string;
+  lastname:string;
+  username_reg:string;
+  password_reg:string;
+  passwordconfirmation:string;
+  email:string;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  logIn(username,password)
+  {
+     this.username = username;
+     this.password = password;
+     //alert(this.username +" "+this.password);
+  
+  }
+
+  register(firstname,lastname,username,email,password,passwordconfirmation){
+
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.username_reg = username;
+    this.email = email;
+    this.password_reg = password;
+    this.passwordconfirmation = passwordconfirmation;
+
+
+  //alert(this.firstname+" "+this.lastname+" "+this.username_reg+" "+this.email+" "+this.password_reg+" "+ this.passwordconfirmation);
+
+
+
+  }
+
+
+  
 
 }
